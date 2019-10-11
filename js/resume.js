48a8a627c4ae0d2895e81572008e4ea6f63bfe1c
jli149 <52467584+jli149@users.noreply.github.com>
2019-10-11 09:23:28 -0700

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -25,4 +25,26 @@
     target: '#sideNav'
   });
 
-})(jQuery); // End of use strict
+})
+<div id="disqus_thread"></div>
+  <script>
+
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+    var disqus_config = function () {
+      this.page.url = https://jli149.github.io/jli149/;  // Replace PAGE_URL with your page's canonical URL variable
+      this.page.identifier = Resume; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+
+    (function () { // DON'T EDIT BELOW THIS LINE
+      var d = document, s = d.createElement('script');
+      s.src = 'https://https-jli149-github-io-jli149.disqus.com/embed.js';
+      s.setAttribute('data-timestamp', +new Date());
+      (d.head || d.body).appendChild(s);
+    })();
+  </script>
+  <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by
+      Disqus.</a></noscript>
+(jQuery); // End of use strict
